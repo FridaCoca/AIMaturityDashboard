212d228e20a4979c7315eb122e772a8bf212cb60
FridaCoca <nayra.mje@gmail.com>
2022-04-27 15:05:23 +0200

    add selection

--- main.py
+++ main.py
@@ -9,24 +9,34 @@ st.set_page_config(page_title="Results Dashboard",
 df = pd.read_excel(
     io='surveyResults.xlsx',
     engine='openpyxl',
-    sheet_name='Sheet1',
+    sheet_name='Sheet2',
     skiprows=0,
-    usecols='E:BP',
+    #usecols='E:BP',
     #nrows=2,
 )
 
-#---- SIDEBAR ----
-strIdentifingColumn = "Unternehmen"
+#---- Sidebar ----
 st.sidebar.header("Bitte hier auswählen:")
+
+strIdentifingColumn = "Unternehmen"
 company = st.sidebar.multiselect(
     "Unternehmen auswählen:",
     options=df[strIdentifingColumn].unique(),
-    #default=df[strIdentifingColumn].unique,
+    #default=df[strIdentifingColumn].unique
+)
+
+strIdentifingColumn = "Gestaltungsdimension"
+dimension = st.sidebar.multiselect(
+    "Unternehmen auswählen:",
+    options=df[strIdentifingColumn].unique(),
+    #default=df[strIdentifingColumn].unique
 )
 
 df_selection = df.query(
-    "Unternehmen == @company"
+    "Unternehmen == @company & Gestaltungsdimension == @dimension"
 )
 
-#st.write(df)
+st.write(df)
 st.dataframe(df_selection)
+
+# ---- Mainpage ---
